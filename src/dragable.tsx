dd53b5b2f269a44204391c679fb7a0f9431f14be
DominikKoniarz <dominik.koniarz.development@gmail.com>
2026-06-04 21:37:41 +0200

    Update dragable styles

--- src/dragable.tsx
+++ src/dragable.tsx
@@ -7,12 +7,15 @@ export default function Draggable() {
     return (
         <MotionDiv ref={containerRef} className="bg-pink/50 size-60 rounded-lg">
             <MotionDiv
-                className="bg-pink size-20 rounded-lg"
+                className="bg-pink grid size-20 cursor-pointer place-items-center rounded-lg text-sm"
                 drag
                 dragMomentum={false}
                 dragConstraints={containerRef}
                 dragElastic={0.05}
-            />
+                whileDrag={{ cursor: "grabbing" }}
+            >
+                Drag me
+            </MotionDiv>
         </MotionDiv>
     );
 }
