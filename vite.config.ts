1f2c0296ff076bc1726d8bf0b4e6fcc6978a0efd
DominikKoniarz <dominik.koniarz.development@gmail.com>
2026-06-09 22:51:46 +0200

    Add github actions deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
-import { defineConfig } from "vite";
-import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+// const repoName = process.env.GITHUB_REPOSITORY?.split("/")[1];
 
 // https://vite.dev/config/
 export default defineConfig({
+    // base:
+    // process.env.GITHUB_PAGES && repoName ? `/${repoName}/` : "/",
     plugins: [
         tailwindcss(),
         react(),
